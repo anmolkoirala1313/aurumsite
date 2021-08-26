687a23be1a5a91a00b17faf5fd181c46cd03fba1
tsurajmaharjan <suraj@canosoft.com.np>
2021-08-26 12:02:21 +0545

    404 page added

--- resources/views/error/404.blade.php
+++ resources/views/error/404.blade.php
@@ -1,1 +1,215 @@
-404 page
+
+<!DOCTYPE html>
+<html lang="en-US" >
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="@if(!empty(@$setting_data->website_description)) {{ucwords(@$setting_data->website_description)}} @else Real Estate @endif">
+<meta name="author" content="">
+<meta name="generator" content="">
+<script>document.documentElement.className = document.documentElement.className + ' yes-js js_active js'</script>
+
+<title>404 Not Found - @if(!empty(@$setting_data->website_name)) {{ucwords(@$setting_data->website_name)}} @else Real Estate @endif </title>
+<link rel="shortcut icon" type="image/x-icon" href="<?php if(@$setting_data->favicon){?>{{asset('/images/uploads/settings/'.@$setting_data->favicon)}}<?php }?>">
+
+
+
+<link rel='stylesheet' id='wp-block-library-css'
+          href="{{asset('assets/frontend/css/style.min.css?ver=5.7.2')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='wp-block-library-theme-css'
+          href="{{asset('assets/frontend/css/theme.min.css?ver=5.7.2')}}"
+          type='text/css' media='all' />
+
+    <link rel='stylesheet' id='dt-animation-css-css'
+          href="{{asset('assets/frontend/plugins/designthemes-core-features/shortcodes/css/animations.css?ver=5.7.2')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='dt-slick-css-css'
+          href="{{asset('assets/frontend/plugins/designthemes-core-features/shortcodes/css/slick.css?ver=5.7.2')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='dt-sc-css-css'
+          href="{{asset('assets/frontend/plugins/designthemes-core-features/shortcodes/css/shortcodes.css?ver=5.7.2')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='rs-plugin-settings-css'
+          href="{{asset('assets/frontend/plugins/revslider/public/assets/css/rs.css?ver=6.3.3')}}"
+          type='text/css' media='all' />
+
+    <link rel='stylesheet' id='js_composer_front-css'
+          href="{{asset('assets/frontend/plugins/js_composer/assets/css/js_composer.min.css?ver=6.5.0')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='bsf-Defaults-css'
+          href="{{asset('assets/frontend/smile_fonts/Defaults/Defaults.css?ver=3.19.8')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='ultimate-style-css'
+          href="{{asset('assets/frontend/plugins/Ultimate_VC_Addons/assets/min-css/style.min.css?ver=3.19.8')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='ultimate-animate-css'
+            href="{{asset('assets/frontend/plugins/Ultimate_VC_Addons/assets/min-css/animate.min.css?ver=3.19.8')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='agencies-css'
+          href="{{asset('assets/frontend/css/themes/style.css?ver=2.7')}}"
+          type='text/css' media='all' />
+
+
+    <link rel='stylesheet' id='agencies-inline-css'
+         href="{{asset('assets/frontend/inlinecss/agencies.css')}}"
+          type='text/css' media='all' />
+
+    <link rel='stylesheet' id='agencies-base-css'
+         href="{{asset('assets/frontend/css/themes/css/base.css?ver=2.7')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='agencies-grid-css'
+        href="{{asset('assets/frontend/css/themes/css/grid.css?ver=2.7')}}"
+          type='text/css' media='all' />
+
+    <link rel='stylesheet' id='agencies-widget-css'
+        href="{{asset('assets/frontend/css/themes/css/widget.css?ver=2.7')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='agencies-layout-css'
+        href="{{asset('assets/frontend/css/themes/css/layout.css?ver=2.7')}}"
+          type='text/css' media='all' />
+    
+    <link rel='stylesheet' id='agencies-custom-class-css'
+        href="{{asset('assets/frontend/css/themes/css/custom-class.css?ver=2.7')}}"
+          type='text/css' media='all' />
+    
+    <link rel='stylesheet' id='custom-font-awesome-css'
+        href="{{asset('assets/frontend/css/themes/css/font-awesome.min.css?ver=2.7')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='pe-icon-7-stroke-css'
+            href="{{asset('assets/frontend/css/themes/css/pe-icon-7-stroke.css')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='stroke-gap-icons-style-css'
+        href="{{asset('assets/frontend/css/themes/css/stroke-gap-icons-style.css')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='icon-moon-css'
+            href="{{asset('assets/frontend/css/themes/css/icon-moon.css')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='material-design-iconic-css'
+        href="{{asset('assets/frontend/css/themes/css/material-design-iconic-font.min.css')}}"
+          type='text/css' media='all' />
+ 
+    <link rel='stylesheet' id='agencies-gutenberg-css'
+        href="{{asset('assets/frontend/css/themes/css/gutenberg.css')}}"
+          type='text/css' media='all' />
+<link rel='stylesheet' id='agencies-notfound-css'  href="{{asset('assets/frontend/inlinecss/notfound.css?ver=2.7')}}" type='text/css' media='all' />
+
+    <link rel='stylesheet' id='agencies-loader-css'
+         href="{{asset('assets/frontend/css/themes/css/loaders.css')}}"
+          type='text/css' media='all' />
+
+ 
+    <link rel='stylesheet' id='agencies-responsive-css'
+        href="{{asset('assets/frontend/css/themes/css/responsive.css')}}"
+          type='text/css' media='all' />
+    <link rel='stylesheet' id='agencies-custom-css'
+        href="{{asset('assets/frontend/css/themes/css/custom.css')}}"
+          type='text/css' media='all' />
+
+    <link rel='stylesheet'
+        href="{{asset('assets/frontend/inlinecss/style.css')}}"
+          type='text/css' media='all' />
+
+    <link rel='stylesheet' id='kirki-inline-styles'
+        href="{{asset('assets/frontend/inlinecss/kirki.css')}}"
+          type='text/css' media='all' />
+
+    <link rel='stylesheet'
+        href="{{asset('assets/frontend/inlinecss/vs_custom.css')}}"
+          type='text/css' media='all' />
+
+
+    <script type='text/javascript'
+            src="{{asset('assets/frontend/js/jquery/jquery.min.js?ver=3.5.1')}}"
+            id='jquery-core-js'></script>
+    <script type='text/javascript'
+            src="{{asset('assets/frontend/js/jquery/jquery-migrate.min.js?ver=3.3.2')}}"
+            id='jquery-migrate-js'></script>
+    
+
+    <script type='text/javascript'
+            src="{{asset('assets/frontend/plugins/Ultimate_VC_Addons/assets/min-js/custom.min.js')}}"
+            id='ultimate-custom-js'></script>
+   
+
+
+   
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id={{@$setting_data->google_analytics}}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '{{@$setting_data->google_analytics}}');
+</script>
+
+    <script type="text/javascript">function setREVStartSize(e) {
+            //window.requestAnimationFrame(function() {
+            window.RSIW = window.RSIW === undefined ? window.innerWidth : window.RSIW;
+            window.RSIH = window.RSIH === undefined ? window.innerHeight : window.RSIH;
+            try {
+                var pw = document.getElementById(e.c).parentNode.offsetWidth,
+                    newh;
+                pw = pw === 0 || isNaN(pw) ? window.RSIW : pw;
+                e.tabw = e.tabw === undefined ? 0 : parseInt(e.tabw);
+                e.thumbw = e.thumbw === undefined ? 0 : parseInt(e.thumbw);
+                e.tabh = e.tabh === undefined ? 0 : parseInt(e.tabh);
+                e.thumbh = e.thumbh === undefined ? 0 : parseInt(e.thumbh);
+                e.tabhide = e.tabhide === undefined ? 0 : parseInt(e.tabhide);
+                e.thumbhide = e.thumbhide === undefined ? 0 : parseInt(e.thumbhide);
+                e.mh = e.mh === undefined || e.mh == "" || e.mh === "auto" ? 0 : parseInt(e.mh, 0);
+                if (e.layout === "fullscreen" || e.l === "fullscreen")
+                    newh = Math.max(e.mh, window.RSIH);
+                else {
+                    e.gw = Array.isArray(e.gw) ? e.gw : [e.gw];
+                    for (var i in e.rl) if (e.gw[i] === undefined || e.gw[i] === 0) e.gw[i] = e.gw[i - 1];
+                    e.gh = e.el === undefined || e.el === "" || (Array.isArray(e.el) && e.el.length == 0) ? e.gh : e.el;
+                    e.gh = Array.isArray(e.gh) ? e.gh : [e.gh];
+                    for (var i in e.rl) if (e.gh[i] === undefined || e.gh[i] === 0) e.gh[i] = e.gh[i - 1];
+
+                    var nl = new Array(e.rl.length),
+                        ix = 0,
+                        sl;
+                    e.tabw = e.tabhide >= pw ? 0 : e.tabw;
+                    e.thumbw = e.thumbhide >= pw ? 0 : e.thumbw;
+                    e.tabh = e.tabhide >= pw ? 0 : e.tabh;
+                    e.thumbh = e.thumbhide >= pw ? 0 : e.thumbh;
+                    for (var i in e.rl) nl[i] = e.rl[i] < window.RSIW ? 0 : e.rl[i];
+                    sl = nl[0];
+                    for (var i in nl) if (sl > nl[i] && nl[i] > 0) { sl = nl[i]; ix = i; }
+                    var m = pw > (e.gw[ix] + e.tabw + e.thumbw) ? 1 : (pw - (e.tabw + e.thumbw)) / (e.gw[ix]);
+                    newh = (e.gh[ix] * m) + (e.tabh + e.thumbh);
+                }
+                if (window.rs_init_css === undefined) window.rs_init_css = document.head.appendChild(document.createElement("style"));
+                document.getElementById(e.c).height = newh + "px";
+                window.rs_init_css.innerHTML += "#" + e.c + "_wrapper { height: " + newh + "px }";
+            } catch (e) {
+                console.log("Failure at Presize of Slider:" + e)
+            }
+            //});
+        };</script>
+
+
+ 
+        
+
+</head>
+<body class="error404 wp-embed-responsive theme-agencies  layout-wide fullwidth-header default sticky-header standard-header header-with-topbar woo-type1 wpb-js-composer js-comp-ver-6.5.0 vc_responsive">
+
+<div class="wrapper type1" style=" background-color:#ffffff;">
+	<div class="container">
+        <div class="center-content-wrapper">
+            <div class="center-content">
+                <div class="error-box square">
+                    <div class="error-box-inner"><h3>Oops!</h3><h2>404</h2><h4>Page Not Found</h4>
+                    </div>
+                </div>
+                <div class="dt-sc-hr-invisible-xsmall"></div>
+                <a class="dt-sc-button filled small"  href="/">Back to Home</a>            
+            </div>
+        </div>
+    </div>    
+</div>
+</body>
+</html>
